cc15290e7b6b76b69c20585fd9d1cce19d7b855b
Infra100 <39909856+Infra100@users.noreply.github.com>
2018-06-04 05:41:03 -0700

    Delete conn2.php

--- conn2.php
+++
@@ -1,7 +0,0 @@
-<?php
-// mysqli
-$mysqli = new mysqli("localhost", "root", "kodc@1234", "demodpt");
-$result = $mysqli("select * from user_details");
-$row = $result->fetch_assoc();
-echo htmlentities($row['_message']);
-?>
